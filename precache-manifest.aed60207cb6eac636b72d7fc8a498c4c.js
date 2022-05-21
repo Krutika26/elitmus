13275b93fa26d1b755e80876f867c26ce5d3dda1
krutika <krutikashahane0@gmail.com>
2022-05-21 16:01:55 +0530

    Updates

---
+++ precache-manifest.aed60207cb6eac636b72d7fc8a498c4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0000388879c77fed843",
+    "url": "/static/css/main.34eb421a.chunk.css"
+  },
+  {
+    "revision": "a0000388879c77fed843",
+    "url": "/static/js/main.a0000388.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "bd4ff739ef5d0f86a0aa",
+    "url": "/static/js/2.bd4ff739.chunk.js"
+  },
+  {
+    "revision": "fa1e30c6e6c64559af32",
+    "url": "/static/js/runtime~main.fa1e30c6.js"
+  },
+  {
+    "revision": "633830980402cd90724c25f02f95c9c7",
+    "url": "/static/media/start.63383098.png"
+  },
+  {
+    "revision": "444b30b5f3866320dc6996f51a7bb703",
+    "url": "/index.html"
+  }
+];
